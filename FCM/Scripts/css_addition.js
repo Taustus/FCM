63798661f80cf6db734c6d17734611126b830191
Zhanibek <zhttaubaldiev@edu.hse.ru>
2021-03-06 18:30:49 +0300

    Move 1st gridview style to external CSS

--- FCM/Scripts/css_addition.js
+++ FCM/Scripts/css_addition.js
@@ -1,7 +1,7 @@
 ﻿function gridViewWithRowAndColumnSelection() {
-    var color = 'yellow', standart_color = 'white';
+    var color = '#ffc000', standart_color = 'white';
     var opacity = 0.2, standart_opacity = 1;
-    $('.gridViewWithRowAndColumnSelection * td').on('mouseover', function () {
+    $('.gridview_with_row_and_column_selection * td').on('mouseover', function () {
         var td = $(this);
         var tr = td.closest('tr').find('td');
         var col = td.closest('table').find('td:nth-child(' + (td.index() + 1) + ')');
@@ -18,13 +18,11 @@
 
         td.css({
             'background': color,
-            'opacity': standart_opacity,
-            'transform': 'scale(1.1)',
-            'transition-duration': '0.5s'
+            'opacity': standart_opacity
         });
 
     });
-    $('.gridViewWithRowAndColumnSelection * td').on('mouseout', function () {
+    $('.gridview_with_row_and_column_selection * td').on('mouseout', function () {
         var td = $(this);
         var tr = td.closest('tr').find('td');
         var col = td.closest('table').find('td:nth-child(' + (td.index() + 1) + ')');
@@ -40,9 +38,7 @@
         });
 
         td.css({
-            'background': standart_color,
-            'transform': 'scale(1)',
-            'transition-duration': '0s'
+            'background': standart_color
         });
     });
 }
